c092328e058bcf768f9e97de7b41743e027cc5d4
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-10-18 16:58:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,38 @@
+const LEVEL_1 = 
+  ["*","*","*","*","*","*","*","*","*","*","*",".","*",
+   "*","S",".",".",".",".",".","*","*",".","*",".","T",
+  "*","*","*","*","*",".",".",".",".",".","*",".","*",
+  "*","*","*","*","*",".","*","*","*",".","*",".","*",
+  "*","*","*","*","*",".","*","*","*","*","*",".","*",
+  "*","*","*","*","*",".","*","*","*","*","*",".","*",
+  "*","*","*","*","*",".",".",".",".",".",".",".","*",
+  "*","*","*","*","*",".","*","*","*","*","*","*","*",
+  "*",".",".",".",".",".",".",".",".",".","*","*","*",
+  "*",".","*","*","*","*","*","*",".",".",".","*","*",
+  "*",".",".",".",".","*","*","*","*","*","*","*","*",
+  "*","*","*","*","*","*","*","*","*","*","*","*","*"]
+
 const lab = document.querySelector('main');
+const areas= document.querySelectorAll('.areas')
 const route= document.querySelector('.route')
 const mur= document.querySelector('.mur');
 
-var i = 0;
-for (; i<100 ; i++){
-    let mur = document.createElement('p');
-    mur.setAttribute('class', 'mur')
-    lab.appendChild(mur) 
-}
-let murs = document.querySelectorAll('.mur')
 
-do{
-  i += 1;
-  murs[1].setAttribute('class','route')
-  murs[11].setAttribute('class','route')
-  murs[13].setAttribute('class','route')
-  murs[14].setAttribute('class','route')
-  murs[15].setAttribute('class','route')
-  murs[16].setAttribute('class','route')
-  murs[21].setAttribute('class','route')
-  murs[23].setAttribute('class','route')
-  murs[26].setAttribute('class','route')
-  murs[31].setAttribute('class','route')
-  murs[33].setAttribute('class','route')
-  murs[35].setAttribute('class','route')
-  murs[36].setAttribute('class','route')
-  murs[43].setAttribute('class','route')
-  murs[50].setAttribute('class','route')
-  murs[51].setAttribute('class','route')
-  murs[52].setAttribute('class','route')
-  murs[53].setAttribute('class','route')
-}
-while(i < 5)
-
-let tresor = document.createElement('p') 
-tresor.setAttribute('class', 'tresor')
-murs[25].innerHTML= '&#128176;'
-murs[25].appendChild(tresor)
-
-let joueur = document.createElement('p')
-joueur.setAttribute('class', 'joueur')
-let index = 0;
-
-murs[index].appendChild(joueur)
-
-document.body.addEventListener("keydown", keyEvent);
-function keyEvent(event) {
-  var x = event.keyCode;
-  if(x === 40){  
-murs[index+=10].appendChild(joueur)
+for (const [key, value] of Object.entries(LEVEL_1)) {
+  console.log(`${key}: ${value}`);
+  let areas = document.createElement('p');
+  areas.setAttribute('class', 'areas')
+  lab.appendChild(areas) 
+  if (value === '*'){
+    areas.style.backgroundColor = 'black'
+    areas.id= 'mur'
   }
-  if(x === 39){  
-  murs[index+=1].appendChild(joueur)
+  if (value === 'T'){
+    areas.style.backgroundColor= 'yellow'
+    areas.id= 'tresor'
   }
-  if(x === 38){  
-  murs[index-=10].appendChild(joueur)
-  }  
-if (x === 37) {  
-murs[index-=1].appendChild(joueur)
+  if (value === 'S'){
+    areas.style.backgroundColor= 'green'
+    areas.id= 'joueur'
   }
 }

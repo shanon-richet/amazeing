57bf0eae4e414e7a6431cedc2300acdf8711314b
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-10-18 18:37:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const LEVEL_1 = 
-  ["*","*","*","*","*","*","*","*","*","*","*",".","*",
-   "*","S",".",".",".",".",".","*","*",".","*",".","T",
+  ["S","*","*","*","*","*","*","*","*","*","*",".","*",
+   "T","*",".",".",".",".",".","*","*",".","*",".","T",
   "*","*","*","*","*",".",".",".",".",".","*",".","*",
   "*","*","*","*","*",".","*","*","*",".","*",".","*",
   "*","*","*","*","*",".","*","*","*","*","*",".","*",
@@ -13,26 +13,57 @@ const LEVEL_1 =
   "*","*","*","*","*","*","*","*","*","*","*","*","*"]
 
 const lab = document.querySelector('main');
-const areas= document.querySelectorAll('.areas')
-const route= document.querySelector('.route')
-const mur= document.querySelector('.mur');
-
 
 for (const [key, value] of Object.entries(LEVEL_1)) {
   console.log(`${key}: ${value}`);
   let areas = document.createElement('p');
   areas.setAttribute('class', 'areas')
   lab.appendChild(areas) 
+  if (value === '.'){
+    areas.style.backgroundColor = 'purple'
+    areas.id= 'mur'
+  }
   if (value === '*'){
     areas.style.backgroundColor = 'black'
-    areas.id= 'mur'
+    areas.id= 'chemin'
   }
   if (value === 'T'){
     areas.style.backgroundColor= 'yellow'
     areas.id= 'tresor'
   }
   if (value === 'S'){
-    areas.style.backgroundColor= 'green'
-    areas.id= 'joueur'
+    var joueur= document.createElement('p')
+    joueur.setAttribute('class', 'joueur')
+    areas.appendChild(joueur)
   }
 }
+
+let index = 0;
+let areas= document.querySelectorAll('.areas')
+let tresor= document.querySelectorAll('tresor')
+
+document.addEventListener('keydown', function(e) {
+  switch (e.keyCode) {
+      case 37: //left arrow
+        if (areas[index-1].id === 'chemin'){
+        areas[index-=1].appendChild(joueur)
+        }
+          break;
+      case 38: //up arrow
+      if (areas[index-13].id === 'chemin'){
+        areas[index-=13].appendChild(joueur)
+      }
+          break;
+      case 39: //right arrow
+      if (areas[index+1].id === 'chemin'){
+        areas[index+=1].appendChild(joueur)
+      }
+          break;
+      case 40: //bottom arrow
+      if(areas[index+13].id === 'chemin'){
+        areas[index+=13].appendChild(joueur)
+      }
+          break;
+  }
+});
+
